012fb56397e07e20a283ad000a2fec71d1733fa8
VCTryo0304 <striker.ripsc3004@gmail.com>
2026-04-18 23:04:39 +0700

    Update aksesibilitas.bundle.js

--- bundle/aksesibilitas.bundle.js
+++ bundle/aksesibilitas.bundle.js
@@ -121,7 +121,7 @@
 
     document.addEventListener("click", function (e) {
         //const toggleBtn = e.target.closest("#accessibilityTab, [data-apr-panel-toggle]");
-        const toggleBtn = e.target.closest("#apr-1303223025-tab, [data-apr-toggle], [data-apr-panel-toggle]"
+        const toggleBtn = e.target.closest(""#accessibilityTab, [data-apr-toggle], [data-apr-panel-toggle]"
 );
 
         if (toggleBtn) {
